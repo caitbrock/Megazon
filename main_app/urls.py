f345b4f389defd6cebc6b002bf4d03bac380c342
Roccay <roccayzhou@gmail.com>
2022-07-25 16:05:04 -0400

    fix image display in detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views  
-
+from django.conf import settings  
+from django.conf.urls.static import static 
 
 urlpatterns = [
   path('', views.home, name='home'),
@@ -18,3 +19,6 @@ urlpatterns = [
   path('products/<int:pk>/delete/', views.ProductDelete.as_view(), name='products_delete'),
 
 ]
+
+if settings.DEBUG:  
+        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  
